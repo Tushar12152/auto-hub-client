bec98bcbb4dbb7a8982bc7de325314ca56480692
Tushar12152 <tusharimran470@gmail.com>
2023-12-18 16:42:33 +0600

    banner added

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -1,8 +1,19 @@
+import Responsive from "../Container/Responsive";
 
 const Banner = () => {
     return (
         <div>
-            
+            <Responsive>
+                <div className="relative">
+                    <div className="bg-[url('https://i.ibb.co/dL0wStm/4.jpg')] min-h-[480px] bg-no-repeat bg-cover text-white">
+                        {/* Overlay div for the black color */}
+                        <div className="absolute inset-0 bg-black opacity-60"></div>
+                        <h1 className="text-white font-bold text-4xl text-center pt-44">Powerful Automotive Directory</h1>
+                        <h3 className="text-white">Create your Automotive website within minutes, no extra plugins required.</h3>
+                    </div>
+                    
+                </div>
+            </Responsive>
         </div>
     );
 };
--- src/Container/Responsive.jsx
+++ src/Container/Responsive.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Responsive = ({children}) => {
     return (
-        <div className='w-[90%] mx-auto'>
+        <div className='w-[98%] mx-auto'>
              {children}
         </div>
     );
