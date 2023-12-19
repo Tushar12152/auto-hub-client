b842c89c0531da1faf6e1a9a5a2f99b6e16f2987
Tushar12152 <tusharimran470@gmail.com>
2023-12-19 16:29:50 +0600

    banner modified

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -5,13 +5,17 @@ const Banner = () => {
         <div>
             <Responsive>
                 <div className="relative">
-                    <div className="bg-[url('https://i.ibb.co/dL0wStm/4.jpg')] min-h-[480px] bg-no-repeat bg-cover text-white">
+                    <div className="bg-[url('https://i.ibb.co/dL0wStm/4.jpg')] min-h-[480px] bg-no-repeat bg-cover text-white relative">
                         {/* Overlay div for the black color */}
-                        <div className="absolute inset-0 bg-black opacity-60"></div>
-                        <h1 className="text-white font-bold text-4xl text-center pt-44">Powerful Automotive Directory</h1>
-                        <h3 className="text-white">Create your Automotive website within minutes, no extra plugins required.</h3>
+                        <div className="absolute inset-0 bg-black opacity-30"></div>
+                        <div>
+                    <h1 className="text-white font-bold text-4xl text-center pt-44">Powerful Automotive Directory</h1>
+                        <h3 className="text-white text-center">Create your Automotive website within minutes, no extra plugins required.</h3>
                     </div>
+                    </div>
+
                     
+
                 </div>
             </Responsive>
         </div>
