572c43207ecfb78ca21463e74e4356aa461eaf1e
Tushar12152 <tusharimran470@gmail.com>
2023-12-20 12:37:00 +0600

    user created

--- src/PRovider/AuthProvider.jsx
+++ src/PRovider/AuthProvider.jsx
@@ -1,19 +1,41 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { PropTypes } from 'prop-types';
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import auth from "../Firebase/Firebase.config";
 
 
 export const AuthContext=createContext(null)
 
 const AuthProvider = ({children}) => {
  const [user,setUser]=useState(null)
- const [loading,setLoading]=useState(false)
+ const [loading,setLoading]=useState(true)
 
+
+
+ const  createUser=(email,password)=>{
+    setLoading(true)
+      return createUserWithEmailAndPassword(auth,email,password)
+
+ }
+
+  useEffect(()=>{
+         const unSubscribe=onAuthStateChanged(auth,currentUser=>{
+             setUser(currentUser)
+             setLoading(false)
+         })
+
+         return ()=>{
+             return unSubscribe()
+         }
+  },[])
 
 
 
     const authInfo={
          user,
-         loading
+         loading,
+         createUser,
+         
     }
     return (
         <AuthContext.Provider value={authInfo}>
