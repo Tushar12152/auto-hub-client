1ac1c5553d7ba4c37267d7e236a1ce4cbfb9856c
Tushar12152 <tusharimran470@gmail.com>
2023-12-27 14:40:36 +0600

    footer added

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,10 +1,33 @@
-
+import Responsive from "./../Container/Responsive";
 const Footer = () => {
-    return (
-        <div>
-             footer
-        </div>
-    );
+  return (
+    <div>
+      <Responsive>
+        <footer className="footer p-10 bg-neutral text-neutral-content">
+          <nav>
+            <header className="footer-title">Services</header>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </nav>
+          <nav>
+            <header className="footer-title">Company</header>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Jobs</a>
+            <a className="link link-hover">Press kit</a>
+          </nav>
+          <nav>
+            <header className="footer-title">Legal</header>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </nav>
+        </footer>
+      </Responsive>
+    </div>
+  );
 };
 
 export default Footer;
